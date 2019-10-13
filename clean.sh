420afd4d197edc6d88a8dd5284316aae09e50a67
Spagno <spagno@redhat.com>
2019-10-13 17:01:25 +0200

    clean tempalte script

---
+++ clean.sh
@@ -0,0 +1,15 @@
+unset HISTFILE
+/sbin/service rsyslog stop
+/sbin/service auditd stop
+package-cleanup --oldkernels --count=1 -y
+yum clean all
+subscription-manager remove --all
+subscription-manager clean
+touch /etc/ssh/testkeytest
+sed -i '/^(HWADDR|UUID)=/d' /etc/sysconfig/network-scripts/ifcfg-e*
+rm -fr /etc/udev/rules.d/70*
+rm -fr /etc/ssh/*key*
+rm -fr ~root/.bash_history
+rm -fr ~root/.ssh/
+rm -fr ~root/anaconda-ks.cfg
+find /var/log -type f -exec rm -fr {} \;
